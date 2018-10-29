2a6a36e1e26c479324396e3deb6a309f01cc13e4
Anton Rehemae <rehemtoha@gmail.com>
2018-10-29 22:58:02 +0200

    fix test, not local start

--- __test__/genDiff.test.js
+++ __test__/genDiff.test.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import genDiff from '../src';
 
 test('difference to be equal', () => {
-  const dir = '/home/anton/Documents/secondProject/__test__/__fixtures__';
+  const dir = './__test__/__fixtures__';
   const before = `${dir}/before.json`;
   const after = `${dir}/after.json`;
   const result = fs.readFileSync(`${dir}/result`).toString();
