00baad9c3f1635b66c54233b3a7c8f68afc1d555
Anton Rehemae <rehemtoha@gmail.com>
2018-10-29 14:29:13 +0200

    add help

---
+++ src/bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import program from 'commander';
+import { version } from '../../package.json';
+
+program
+  .version(version)
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type] ', 'Output format')
+  .parse(process.argv);
