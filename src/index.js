500662d5e7aac706441a79bedd1f6f7b7ee9f171
Anton Rehemae <rehemtoha@gmail.com>
2018-10-30 17:54:12 +0200

    remove if

--- src/index.js
+++ src/index.js
@@ -11,26 +11,35 @@ const genDiff = (firstConfig, secondConfig) => {
   const obj1 = getData(firstConfig);
   const obj2 = getData(secondConfig);
 
-  const changeData = key => `+ ${key}: ${obj2[key]}
-  - ${key}: ${obj1[key]}`;
+  const isSharedKey = key => _.has(obj1, key) && _.has(obj2, key);
+  const hasSameValue = key => obj1[key] === obj2[key];
+  const isNewKey = key => _.has(obj2, key);
 
-  const removeData = key => `- ${key}: ${obj1[key]}`;
-
-  const addDate = key => `+ ${key}: ${obj2[key]}`;
-
-  const findDif = (key) => {
-    const keyIsShared = _.has(obj1, key) && _.has(obj2, key);
-    if (keyIsShared) {
-      return obj1[key] === obj2[key]
-        ? `  ${key}: ${obj2[key]}` : changeData(key);
-    }
-    return _.has(obj2, key) ? addDate(key) : removeData(key);
-  };
+  const propertyActions = [
+    {
+      check: key => isSharedKey(key) && hasSameValue(key),
+      process: key => `  ${key}: ${obj2[key]}`,
+    },
+    {
+      check: key => isSharedKey(key) && !hasSameValue(key),
+      process: key => `+ ${key}: ${obj2[key]}\n  - ${key}: ${obj1[key]}`,
+    },
+    {
+      check: key => isNewKey(key),
+      process: key => `+ ${key}: ${obj2[key]}`,
+    },
+    {
+      check: key => !isNewKey(key),
+      process: key => `- ${key}: ${[obj1[key]]}`,
+    },
+  ];
+  const getPropertyAction = key => propertyActions.find(({ check }) => check(key));
 
   const keys = _.union(_.keys(obj1), _.keys(obj2));
-  return `{${keys.reduce((acc, elem) => `${acc}
-  ${findDif(elem)}`, '')}
-}`;
+  return `{${keys.reduce((acc, elem) => {
+    const { process } = getPropertyAction(elem);
+    return `${acc}\n  ${process(elem)}`;
+  }, '')}\n}`;
 };
 
 export default genDiff;
