0ea4b899bdd4ebff039c5182e0292974befa3b99
Anton Rehemae <rehemtoha@gmail.com>
2018-10-30 13:37:52 +0200

    fix lint task

--- src/parsers.js
+++ src/parsers.js
@@ -4,7 +4,7 @@ import ini from 'ini';
 const parseManager = {
   json: data => JSON.parse(data),
   yml: data => yaml.safeLoad(data),
-  ini: data =>ini.decode(data)
+  ini: data => ini.decode(data),
 };
 
 export default (data, type) => parseManager[type](data);
