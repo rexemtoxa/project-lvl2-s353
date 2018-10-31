88eb7c90c506786b4603cc8fdd83cac138686782
Anton Rehemae <rehemtoha@gmail.com>
2018-10-31 20:49:00 +0200

    fix asc

--- src/genAst.js
+++ src/genAst.js
@@ -1,4 +1,3 @@
-/* eslint no-use-before-define: ["error", { "variables": false }] */
 import _ from 'lodash';
 
 const isSharedKey = (key, obj1, obj2) => _.has(obj1, key) && _.has(obj2, key);
@@ -8,7 +7,7 @@ const areObjects = (key, obj1, obj2) => _.isObject(obj1[key]) && _.isObject(obj2
 const propertyActions = [
   {
     check: (key, obj1, obj2) => areObjects(key, obj1, obj2),
-    process: (key, obj1, obj2) => ({ key, type: 'parent', children: genAst(obj1[key], obj2[key]) }),
+    process: (key, obj1, obj2, f) => ({ key, type: 'parent', children: f(obj1[key], obj2[key]) }),
   },
   {
     check: (key, obj1, obj2) => isSharedKey(key, obj1, obj2) && hasSameValue(key, obj1, obj2),
@@ -37,7 +36,7 @@ const genAst = (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2));
   return keys.map((key) => {
     const { process } = getPropertyAction(key, obj1, obj2);
-    return process(key, obj1, obj2);
+    return process(key, obj1, obj2, genAst);
   });
 };
 export default genAst;
