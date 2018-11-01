565ca930d2c2b72c654484732354426c87ce88bd
Anton Rehemae <rehemtoha@gmail.com>
2018-11-01 13:01:01 +0200

    fix name for test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,9 +3,9 @@ import genDiff from '../src';
 
 test('difference two JSON files', () => {
   const dir = './__tests__/__fixtures__';
-  const pathFirstConfig = `${dir}/before.json`;
-  const pathSecondConfig = `${dir}/after.json`;
-  const result = fs.readFileSync(`${dir}/result`).toString();
+  const pathFirstConfig = `${dir}/beforeDefaultFormat.json`;
+  const pathSecondConfig = `${dir}/afterDefaultFormat.json`;
+  const result = fs.readFileSync(`${dir}/resultDefaultFormat`).toString();
 
   const actual = genDiff(pathFirstConfig, pathSecondConfig);
   expect(actual).toBe(result);
@@ -13,9 +13,9 @@ test('difference two JSON files', () => {
 
 test('difference two yaml files', () => {
   const dir = './__tests__/__fixtures__';
-  const pathFirstConfig = `${dir}/before.yml`;
-  const pathSecondConfig = `${dir}/after.yml`;
-  const result = fs.readFileSync(`${dir}/result`).toString();
+  const pathFirstConfig = `${dir}/beforeDefaultFormat.yml`;
+  const pathSecondConfig = `${dir}/afterDefaultFormat.yml`;
+  const result = fs.readFileSync(`${dir}/resultDefaultFormat`).toString();
 
   const actual = genDiff(pathFirstConfig, pathSecondConfig);
   expect(actual).toBe(result);
@@ -23,9 +23,9 @@ test('difference two yaml files', () => {
 
 test('difference two ini files', () => {
   const dir = './__tests__/__fixtures__';
-  const pathFirstConfig = `${dir}/before.ini`;
-  const pathSecondConfig = `${dir}/after.ini`;
-  const result = fs.readFileSync(`${dir}/result`).toString();
+  const pathFirstConfig = `${dir}/beforeDefaultFormat.ini`;
+  const pathSecondConfig = `${dir}/afterDefaultFormat.ini`;
+  const result = fs.readFileSync(`${dir}/resultDefaultFormat`).toString();
 
   const actual = genDiff(pathFirstConfig, pathSecondConfig);
   expect(actual).toBe(result);
